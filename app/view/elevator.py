d7715341dc5970906a8c5102ee958f2d63aabc4f
llittleTomato <shikeyi627@163.com>
2018-07-27 22:40:44 +0800

    表格显示数据功能已走通，重构注册页面，未完成页面css样式

--- app/view/elevator.py
+++ app/view/elevator.py
@@ -3,9 +3,11 @@ __author__ = 'sky'
 from . import view
 from flask import render_template
 from flask_login import login_required
+from app.models.elevator import Elevator
 
 
 @view.route('/elevator/')
 @login_required
 def elevator():
-	return render_template('elevatorManage.html')
+    elevators = Elevator.query.all()
+    return render_template('elevatorManage.html', elevators=elevators)
