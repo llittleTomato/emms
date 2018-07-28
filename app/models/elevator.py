4a98d719acfd5c1b29ec873c2e19079debc6af47
llittleTomato <shikeyi627@163.com>
2018-07-28 23:12:16 +0800

    完善了页面布局

--- app/models/elevator.py
+++ app/models/elevator.py
@@ -6,9 +6,10 @@ from app.models.base import Base
 
 class Elevator(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)          # 数据编号
-    ein = Column(String(6), unique=True, nullable=False)                # 识别码
+    # ein = Column(String(6), unique=True, nullable=False)                # 识别码
     registerno = Column(String(16), unique=True, nullable=False)        # 注册代码
     version = Column(String(30), nullable=False)                        # 型号
-    manufacture_company = Column(String(30), nullable=False)            # 制造单位
-    install_company = Column(String(30), nullable=False)                # 安装单位
-    reform_company = Column(String(30), nullable=True)                  # 改造大修单位
+    company = Column(String(30), nullable=False)            # 制造单位
+    # manufacture_company = Column(String(30), nullable=False)            # 制造单位
+    # install_company = Column(String(30), nullable=False)                # 安装单位
+    # reform_company = Column(String(30), nullable=True)                  # 改造大修单位
