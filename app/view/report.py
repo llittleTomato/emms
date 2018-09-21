665ed2c07b6f4256302521b499ec0fd2bcfaa9d0
llittletomato <shikeyi627@163.com>
2018-09-21 16:02:31 +0800

    完善报告生成功能，pdf显示功能 on ubuntu ThinkPad

--- app/view/report.py
+++ app/view/report.py
@@ -5,15 +5,6 @@ from flask import render_template, request
 from flask_login import login_required
 
 
-@view.route('/report_init/', methods=['GET', 'POST'])
-@login_required
-def report_init():
-    if request.method == 'POST':
-        return render_template('report/reportGeneration.html')
-    else:
-        return render_template('report/reportInit.html')
-
-
 @view.route('/report_generation/', methods=['GET', 'POST'])
 @login_required
 def report_generation():
@@ -29,7 +20,7 @@ def report_manage():
     return render_template('report/reportManage.html')
 
 
-@view.route('/report_show/')
+@view.route('/report_show/', methods=['GET'])
 @login_required
 def report_show():
     return render_template('report/reportShow.html')
