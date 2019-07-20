cf931d244e4fc0cb56381c8b1f56c23b8526d905
llittletomato <shikeyi627@163.com>
2019-07-20 22:27:24 +0800

    完善index页面的数量显示,修正login页面bug

--- app/view/index.py
+++ app/view/index.py
@@ -1,5 +1,7 @@
 from sqlalchemy import and_
 from app.models.elevator import ElevatorRoom
+from app.models.employee import Employee
+from app.models.report import ReportElevatorRoom
 from . import view
 from flask import render_template
 from flask_login import login_required, current_user
@@ -9,4 +11,6 @@ from flask_login import login_required, current_user
 @login_required
 def index():
     ele_count = ElevatorRoom.query.filter(and_(ElevatorRoom.maintenanceCompany == current_user.company, ElevatorRoom.status==1)).count()
-    return render_template('index.html', count=ele_count)
+    employee_count = Employee.query.filter(and_(Employee.company == current_user.company, Employee.status == 1)).count()
+    report_count = ReportElevatorRoom.query.filter(and_(ReportElevatorRoom.maintenanceCompany == current_user.company, ReportElevatorRoom.status == 1)).count()
+    return render_template('index.html', ele_count=ele_count, employee_count=employee_count, report_count=report_count)
